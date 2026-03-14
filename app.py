852dc311852919db66c0c3f31f1a78e56b18b69b
Delta Student <a94731386@gmail.com>
2026-03-14 17:06:52 +0530

    Fix pkg_resources import issue

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import os
 os.environ['TF_USE_LEGACY_KERAS'] = '1'
 
+# Fix for pkg_resources missing in newer Python versions
+try:
+    import pkg_resources
+except ImportError:
+    import setuptools
+    import pkg_resources
+
 import streamlit as st
 import tensorflow_hub as hub
 import tf_keras
